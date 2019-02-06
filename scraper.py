e183509c4e8f19823bcb91d1308731139d3ad012
James Polley <jp@jamezpolley.com>
2019-02-06 16:26:27 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,7 +10,8 @@ hostnames = ["www.yahoo.com", "ina.gl"]
 for host in hostnames:
   data = {"host": host}
   output = subprocess.check_output(
-    ["openssl", "s_client", "-showcerts", "-CAfile", "/etc/ssl/certs/ca-certificates.crt", "-connect", "%s:443" % host, "-servername", host, "<", "/dev/null"],)
+    ["openssl", "s_client", "-showcerts", "-CAfile", "/etc/ssl/certs/ca-certificates.crt", "-connect", "%s:443" % host, "-servername", host, "<", "/dev/null"],
+    stdin=open("/dev/null")
   data["output"] = output
   pprint.pprint(data)
   scraperwiki.sqlite.save(unique_keys=['host'], data=data)
