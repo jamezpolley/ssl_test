125dc56fe1121786b7e19df480c36110b18559fa
James Polley <jp@jamezpolley.com>
2019-02-06 16:11:13 +1100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -10,11 +10,9 @@ data = {}
 
 for host in hostnames:
   data["host"] = host
-  output = subprocess.Popen(
-    ["openssl", "s_client", "-showcerts", "-CAfile", "/etc/ssl/certs/ca-certificates.crt", "-connect", "%s:443" % host, "-servername", host],
-    stderr=PIPE, stdout=PIPE)
-  for value in ["returncode", "stdout", "stderr"]:
-    data[value] = getattr(output, value)
+  output = subprocess.check_output(
+    ["openssl", "s_client", "-showcerts", "-CAfile", "/etc/ssl/certs/ca-certificates.crt", "-connect", "%s:443" % host, "-servername", host],)
+    data["output"] = output
   pprint.pprint(data)
   scraperwiki.sqlite.save(unique_keys=['host'], data=data)
 
